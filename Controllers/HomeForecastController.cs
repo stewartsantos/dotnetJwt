a712f3e445a4a2a1a5a812d1894201f3f89a27f8
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-09-29 17:07:23 -0300

    rota home

---
+++ Controllers/HomeForecastController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiJwt.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public Hashtable Index()
+        {
+            var defaultHath = new Hashtable();
+            defaultHath.Add("Mensagem", "Olá bem a minha API do torne-se um programado com JWT");
+            return defaultHath;
+        }
+    }
+}
