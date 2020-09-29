2b499fd636f11e36df0bfdabaef85e213ff05176
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-09-29 18:19:31 -0300

    fix

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -30,7 +30,7 @@ namespace ApiJwt.Services
                 Subject = new ClaimsIdentity(new Claim[]
                 {
                     new Claim(ClaimTypes.Name, user.Username),
-                    new Claim("Store", user.Role)
+                    new Claim("ApiJwt", user.Role)
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
